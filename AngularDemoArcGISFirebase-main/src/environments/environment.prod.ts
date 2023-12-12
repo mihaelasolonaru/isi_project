898c95f2fa4413b619401931fcc7725c7e30cf5f
unknown <alexstefan_nedelcu@yahoo.com>
2023-12-12 11:48:41 +0200

    Setup proiect ISI

---
+++ AngularDemoArcGISFirebase-main/src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDQGsFwljicUwihZFiyRg8FpQxnPKVzR0k",
+
+    authDomain: "totallyspies-89217.firebaseapp.com",
+  
+    projectId: "totallyspies-89217",
+  
+    storageBucket: "totallyspies-89217.appspot.com",
+  
+    messagingSenderId: "1055565014119",
+  
+    appId: "1:1055565014119:web:3c673283b7f4775abfa0c2",
+  
+    measurementId: "G-C3CV3XWZM1"
+  
+  },
+  production: true
+};
---
+++ AngularDemoArcGISFirebase-main/src/environments/environment.ts
@@ -0,0 +1,32 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDQGsFwljicUwihZFiyRg8FpQxnPKVzR0k",
+
+    authDomain: "totallyspies-89217.firebaseapp.com",
+  
+    projectId: "totallyspies-89217",
+  
+    storageBucket: "totallyspies-89217.appspot.com",
+  
+    messagingSenderId: "1055565014119",
+  
+    appId: "1:1055565014119:web:3c673283b7f4775abfa0c2",
+  
+    measurementId: "G-C3CV3XWZM1"
+  
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
